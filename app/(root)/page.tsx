c4960c96d6fea3f77042d1a24a9a4ecda177c8e1
Maurice Consultant <mainna62@outlook.com>
2024-08-18 18:55:40 +0300

    initialize file and folder structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: 'Maurice' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Manage your account and transactions"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
